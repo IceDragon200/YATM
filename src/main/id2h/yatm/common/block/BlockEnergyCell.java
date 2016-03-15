f8295b703c44b03afaefb0f4525304cb395e6d2a
Corey Powell <mistdragon100@gmail.com>
2016-03-14 22:16:52 -0500

    Remove unused import

--- src/main/id2h/yatm/common/block/BlockEnergyCell.java
+++ src/main/id2h/yatm/common/block/BlockEnergyCell.java
@@ -34,7 +34,6 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.IIcon;
